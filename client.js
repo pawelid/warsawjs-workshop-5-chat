2f5e9b90ad1e5bb1f3b8875a4d588fa7639b3d24
Pawlik <pawel.idczak@gmail.com>
2017-04-23 10:51:22 +0200

    es6

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const connection = require('socket.io-client')('http://localhost:3000');
 
-connection.on('message', function (message) {
-  console.log('server said: %s', message.body);
+connection.on('message', function ( { body }) {
+  console.log('server said: %s', body);
 });
